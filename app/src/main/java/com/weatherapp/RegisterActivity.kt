f9e725b1b61c702f8d3d4eba74ef99bbe4998767
LucianoSegundo <lufilho2020@gmail.com>
2024-12-04 21:15:19 -0300

    “Prática 01 – Parte 4 – Criação”

--- app/src/main/java/com/weatherapp/RegisterActivity.kt
+++ app/src/main/java/com/weatherapp/RegisterActivity.kt
@@ -2,7 +2,6 @@ package com.weatherapp
 
 import android.app.Activity
 import android.content.Intent
-import android.content.Intent.FLAG_ACTIVITY_SINGLE_TOP
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
@@ -25,23 +24,22 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.weatherapp.ui.theme.WeatherAppTheme
 
-class LoginActivity : ComponentActivity() {
+class RegisterActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             WeatherAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    LoginPage(
+                    Registro(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -52,74 +50,93 @@ class LoginActivity : ComponentActivity() {
 
 @Preview(showBackground = true)
 @Composable
-fun LoginPage(modifier: Modifier = Modifier) {
+fun Registro( modifier: Modifier = Modifier) {
+    var nome by rememberSaveable { mutableStateOf("") }
     var email by rememberSaveable { mutableStateOf("") }
-    var password by rememberSaveable { mutableStateOf("") }
+    var senha by rememberSaveable { mutableStateOf("") }
+    var confSenha by rememberSaveable { mutableStateOf("") }
     val activity = LocalContext.current as? Activity
+
     Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
-        horizontalAlignment = CenterHorizontally,
-        modifier = modifier.padding(16.dp).fillMaxSize(),
+        modifier =  modifier.padding(8.dp)
     ) {
-        Text(
-            text = "Bem-vindo/a!",
-            fontSize = 24.sp
-        )
-        Spacer(modifier = modifier.size(4.dp))
+
+    Text(
+        text = "Registro",
+
+    )
         OutlinedTextField(
-            value = email,
-            label = { Text(text = "Digite seu e-mail") },
+            value = nome,
+            label = { Text(text = "Nome") },
             modifier = modifier.fillMaxWidth(),
-            onValueChange = { email = it }
+            onValueChange = { nome = it}
+
         )
-        Spacer(modifier = modifier.size(24.dp))
+
         OutlinedTextField(
-            value = password,
-            label = { Text(text = "Digite sua senha") },
-            modifier = modifier.fillMaxWidth(),
-            onValueChange = { password = it },
+            value =  email,
+            label =  { Text(text = "Email") },
+            modifier =  modifier.fillMaxWidth(),
+            onValueChange =  {email = it}
+        )
+
+        OutlinedTextField(
+            value = senha,
+            label = { Text(text = "Senha") },
+            modifier =  modifier.fillMaxWidth(),
+            onValueChange = {senha = it},
             visualTransformation = PasswordVisualTransformation()
+
         )
-        Spacer(modifier = modifier.size(4.dp))
-        Row(modifier = modifier) {
+
+        OutlinedTextField(
+            value = confSenha,
+            label = { Text(text = "Confirmar Senha") },
+            modifier = modifier.fillMaxWidth(),
+            onValueChange = {confSenha= it},
+            visualTransformation = PasswordVisualTransformation()
+
+        )
+
+        Row {
             Button(
                 onClick = {
-                    Toast.makeText(activity, "Login OK!", Toast.LENGTH_LONG).show();
-                    activity?.startActivity(
-                        Intent(activity, MainActivity::class.java).setFlags(
-                            FLAG_ACTIVITY_SINGLE_TOP
-                        )
-                    )
+                    Toast.makeText(activity, "Voltar", Toast.LENGTH_LONG).show()
 
-
-                } ,
-                enabled = email.isNotEmpty() && password.isNotEmpty()
-
-            ) {
-                Text("Login")
+                    activity?.finish()
+                },
+            ){
+                Text("voltar")
             }
-
             Spacer(modifier = modifier.size(24.dp))
+
+            Button(onClick = {
+                senha= "";
+                nome = "";
+                email ="";
+                confSenha ="";
+
+                Toast.makeText(activity, "formulario Limpo", Toast.LENGTH_SHORT).show()
+            }) {
+                Text("Limpar")
+            }
+            Spacer(modifier = modifier.size(24.dp))
+
+
             Button(
                 onClick = {
-                    activity?.startActivity(
-                        Intent(activity, RegisterActivity::class.java).setFlags(
-                            FLAG_ACTIVITY_SINGLE_TOP
-                        )
-                    )
+                    Toast.makeText(activity, "Registrado com sucesso", Toast.LENGTH_LONG).show()
 
-
-                }
+                    activity?.finish()
+                },
+                enabled =  nome.isNotEmpty() && email.isNotEmpty() && senha.isNotEmpty() && confSenha.isNotEmpty() && senha.equals(confSenha)
             ) {
                 Text("Registrar")
             }
-
-            Spacer(modifier = modifier.size(24.dp))
-            Button(
-                onClick = { email = ""; password = "" }
-            ) {
-                Text("Limpar")
-            }
         }
+
     }
 }
+
