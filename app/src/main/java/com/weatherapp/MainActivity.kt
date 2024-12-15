b990099c29bbccb4653c5523a2d4df511cb9d3b9
LucianoSegundo <103046717+LucianoSegundo@users.noreply.github.com>
2024-12-15 11:55:19 -0300

    Pratica 02 -- Parte 1 -- criação das paginas

--- app/src/main/java/com/weatherapp/MainActivity.kt
+++ app/src/main/java/com/weatherapp/MainActivity.kt
@@ -1,26 +1,13 @@
 package com.weatherapp
 
-import android.app.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.weatherapp.ui.theme.WeatherAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,9 +17,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             WeatherAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    homepage(
+                    HomePage(
 
-                        modifier = Modifier.padding(innerPadding)
+
                     )
                 }
             }
@@ -40,28 +27,3 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Preview(showBackground = true)
-@Composable
-fun homepage( modifier: Modifier = Modifier) {
-    val activity = LocalContext.current as? Activity
-
-
-    Column(
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = modifier.padding(24.dp).fillMaxWidth()
-    ) {
-
-    Text(
-        text = "Boas vistas a primeira homepage android",
-        modifier = modifier
-    )
-        Button(
-            onClick ={
-                activity?.finish()
-            }
-        ) {
-            Text("Sair")
-        }
-    }
-}
